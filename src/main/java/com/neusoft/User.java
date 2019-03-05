b86dc13b0d385f8647ab802b6896428a56ea643f
‘wangzhuoqi’ <924170576@qq.com>
2019-03-05 14:25:49 +0800

    一个练习 gaiyixia叶琦琪的

--- src/main/java/com/neusoft/User.java
+++ src/main/java/com/neusoft/User.java
@@ -4,5 +4,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("改了一下");
+        System.out.println("叶琦琪啊叶琦琪");
     }
 }
